1d07c422d479f5053911bbfde72de340ea3a5dcc
Ryan Yang <javathon@gmail.com>
2019-08-16 13:37:45 -0700

    Updates

---
+++ precache-manifest.f4a406431d6cfd94e3610193dbe5ae1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e622a4cfce4f16aa2e6d9a438f174a2",
+    "url": "/interactive-world-stats-display/index.html"
+  },
+  {
+    "revision": "96fcde4e360bb2954282",
+    "url": "/interactive-world-stats-display/static/css/main.2dd085a9.chunk.css"
+  },
+  {
+    "revision": "85f134db9336a723c1d3",
+    "url": "/interactive-world-stats-display/static/js/2.8a5c37ab.chunk.js"
+  },
+  {
+    "revision": "96fcde4e360bb2954282",
+    "url": "/interactive-world-stats-display/static/js/main.4df71591.chunk.js"
+  },
+  {
+    "revision": "0693ad2f561c7167f556",
+    "url": "/interactive-world-stats-display/static/js/runtime~main.062ce2ba.js"
+  },
+  {
+    "revision": "d5f1a89b2dd95c6856a6066892992c15",
+    "url": "/interactive-world-stats-display/static/media/icon3.d5f1a89b.png"
+  }
+]);
